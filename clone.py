b346fe0a6a92f7482131281b287f5df901e7dcc0
Kawshlya Wijarthna <96597172+Kawshlya0day@users.noreply.github.com>
2022-11-13 16:45:24 +0530

    Initial commit

---
+++ clone.py
@@ -0,0 +1,9 @@
+first_name = 'kawshlya'
+surname = 'wijarthna'
+
+full_name = first_name.capitalize() +' '+ surname
+
+print(full_name)
+print(len(first_name))
+print(len(surname))
+print(full_name.endswith('wijarthna'))
---
+++ my_app.py
@@ -0,0 +1,23 @@
+user_name='pasandul'
+user_vehicle_type='car'
+vehicle_fuel_tank_capacity="60"
+fuel_type="95"
+fuel_price="450"
+previous_meter_reading="1000"
+current_meter_reading="3000"
+fuel_pumped_now="60"
+
+subtraction=1000 - 3000
+mutiplication= 450 * 60
+division='subtraction' / 60
+
+
+
+
+print('hi' + user_name )
+print(previous_meter_reading)
+print(current_meter_reading)
+print(fuel_type)
+print(fuel_price)
+print(mutiplication)
+print(division)
