ca28b1186307bf2da1cae2084d870ff719626a32
Kawshlya Wijarthna <96597172+Kawshlya0day@users.noreply.github.com>
2022-11-13 20:59:54 +0530

    fix
    fix

--- clone.py
+++
@@ -1,9 +0,0 @@
-first_name = 'kawshlya'
-surname = 'wijarthna'
-
-full_name = first_name.capitalize() +' '+ surname
-
-print(full_name)
-print(len(first_name))
-print(len(surname))
-print(full_name.endswith('wijarthna'))
