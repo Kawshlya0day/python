4d41c6b5ccd35f35578cb5ad332f0b78f7484f7e
Kawshlya Wijarthna <96597172+Kawshlya0day@users.noreply.github.com>
2022-11-13 20:58:01 +0530

    fix
    thfg

--- my_app.py
+++ my_app.py
@@ -1,20 +1,35 @@
-user_name='pasandul'
-user_vehicle_type='car'
-vehicle_fuel_tank_capacity="60"
-fuel_type="95"
-fuel_price="450"
-previous_meter_reading="1000"
-current_meter_reading="3000"
-fuel_pumped_now="60"
+#please enter your name
+user_name='kawshlya'
 
-subtraction=1000 - 3000
-mutiplication= 450 * 60
-division='subtraction' / 60
+#Vehicle Type 
+user_vehicle_type='Bike'
+
+#Vehicle Fuel Tank Capacity
+vehicle_fuel_tank_capacity="56"
+
+#Fuel Type
+fuel_type="92"
+
+#Fuel Price
+fuel_price="370"
+
+#Previous Meter Reading(Km)
+previous_meter_reading="2300"
+
+#Current Meter Reading(Km)
+current_meter_reading="5100"
+
+#How many Fuel you pumped Now (L)
+fuel_pumped_now="56"
+
+subtraction=2300 - 5100
+mutiplication= 370 * 56
+division ='subtraction' / 56
 
 
 
 
-print('hi' + user_name )
+print('hi' +' '+user_name )
 print(previous_meter_reading)
 print(current_meter_reading)
 print(fuel_type)
