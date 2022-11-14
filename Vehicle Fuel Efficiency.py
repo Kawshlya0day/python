1b410ac94c33e08b2817687774d54de56c614973
Kawshlya Wijarthna <96597172+Kawshlya0day@users.noreply.github.com>
2022-11-14 22:13:55 +0530

    giv
    ui

--- Vehicle Fuel Efficiency.py
+++ Vehicle Fuel Efficiency.py
@@ -1,31 +1,32 @@
 #please enter your name
-user_name='kawshlya'
+user_name=input('Please enter your name here')
 
 #Vehicle Type 
-user_vehicle_type='Bike'
+user_vehicle_type=input('Whats your vehicle type? Car , Bike or SUVe')
 
 #Vehicle Fuel Tank Capacity
-vehicle_fuel_tank_capacity="56"
+vehicle_fuel_tank_capacity=input('Mention the fuel tank capacity')
 
 #Fuel Type
-fuel_type="92"
+fuel_type=input('Whats your fuel type? 92 , 95 or Super Diesel')
 
 #Fuel Price
-fuel_price="370"
+fuel_price=input('Enter fuel price')
 
 #Previous Meter Reading(Km)
-previous_meter_reading="2300"
+previous_meter_reading=input('whats your previous meter reading?')
 
 #Current Meter Reading(Km)
-current_meter_reading="5100"
+current_meter_reading=input('whats your current meter reading?')
 
 #How many Fuel you pumped Now (L)
-fuel_pumped_now="56"
+fuel_pumped_now=input('How many fuel you pumped now')
+
+
 
 subtraction=2300 - 5100
 mutiplication= 370 * 56
 division ='subtraction' / 56
-
 
 
 
